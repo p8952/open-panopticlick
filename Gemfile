8c8657b542ba5907496fc40bb02d54683486ff02
Peter Wilmott <p@p8952.info>
2013-08-10 02:08:32 +0100

    Add a simple counter using redis

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 ruby "2.0.0"
 gem 'rake'
+gem 'redis'
 gem 'sinatra'
 gem 'useragent'
--- open_panopticlick.rb
+++ open_panopticlick.rb
@@ -1,6 +1,14 @@
 require 'digest/md5'
+require 'redis'
 require 'sinatra'
 require 'useragent'
+
+if ENV["REDISCLOUD_URL"]
+  uri = URI.parse(ENV["REDISCLOUD_URL"])
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  $redis = Redis.new(:host => "127.0.0.1", :port => 6379)
+end
 
 $id_vars = Hash.new
 $id_vars[:os] = 0
