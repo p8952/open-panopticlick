9eed0d7d0a8622b9c794a9a48654689eec8997d7
Peter Wilmott <p@p8952.info>
2013-08-10 01:55:58 +0100

    Simple useragent based fingerprint

--- open_panopticlick.rb
+++ open_panopticlick.rb
@@ -1,4 +1,14 @@
+require 'digest/md5'
 require 'sinatra'
+require 'useragent'
+
+$id_vars = Hash.new
+$id_vars[:os] = 0
+$id_vars[:platform] = 0
+$id_vars[:browser] = 0
+$id_vars[:version] = 0
+$id_vars[:build] = 0
+$id_vars[:localization] = 0
 
 class OpenPanopticlick < Sinatra::Application
   
